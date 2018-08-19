fca6d4cdd98e62b9d4e6e9fe77684a388176da0c
Michał Kajetan Fadrowski <michal.fadrowski@gmail.com>
2018-08-19 02:46:53 +0200

    css
    podpięte pliki css i js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,10 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
+Rails.application.config.assets.precompile +=%w(style.css)
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( all.min.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
